6624300b3916fe9ba36594a3fccb28e02c8a1f0b
santoshgopane <santoshgopane68@gmail.com>
2022-11-28 00:12:02 +0530

    New directory

---
+++ Consistent/L22Problem1.cpp
@@ -0,0 +1,16 @@
+// Character Arrays &String Lecture.
+#include <bits/stdc++.h>
+using namespace std;
+
+void testFunction()
+{
+    cout << "Printing shit!";
+    return;
+}
+
+int main()
+{
+    cout << "Santosh was here first :)" << endl;
+    testFunction();
+    return 0;
+}
