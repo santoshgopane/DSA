c78532c742a2c1437093d558cb506865dc775456
santoshgopane <santoshgopane68@gmail.com>
2022-12-19 23:59:05 +0530

    String Compression problem

--- Consistent/L22Problem9.cpp
+++ Consistent/L22Problem9.cpp
@@ -4,7 +4,34 @@ using namespace std;
 
 int compress(vector<char> &chars)
 {
-    return 1;
+    int letterCount = 1;
+    for (int i = 0; i < chars.size(); i++)
+    {
+        for (int j = i; j < chars.size() - 1 && chars[j] == chars[j + 1]; j++)
+        {
+            letterCount++;
+        }
+        // Insert the digit
+        // Erase the letters from digiLen to i+lettercount pos
+        int digit = letterCount, digiLen = 0;
+        if (letterCount > 1)
+        {
+            while (digit > 0)
+            {
+                chars.insert(chars.begin() + i + 1, (digit % 10) + '0');
+                digit = digit / 10;
+                digiLen++;
+            }
+            chars.erase(chars.begin() + i + digiLen + 1, chars.begin() + i + letterCount + digiLen);
+        }
+
+        i += digiLen;
+        letterCount = 1;
+    }
+    cout << "Final:" << endl;
+    for (char i : chars)
+        cout << i << " ";
+    return chars.size();
 }
 
 int main()
@@ -12,11 +39,32 @@ int main()
     vector<char> v;
     v.push_back('a');
     v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
+    v.push_back('a');
     v.push_back('b');
     v.push_back('b');
+    v.push_back('b');
+
     v.push_back('c');
     v.push_back('c');
     v.push_back('c');
+    // v.insert(v.begin() + 1, 5 + '0');
+    // v.erase(v.begin(), v.begin() + 2);
+
     for (auto i : v)
         cout << i << " ";
+    cout << endl;
+
+    cout << endl
+         << "Something:"
+         << compress(v);
 }
--- Test.cpp
+++ Test.cpp
@@ -11,16 +11,32 @@ void testFunction(int a[], int size)
 int main()
 {
     int size;
-    int arr[99];
+    // int arr[99];
+    int digit = 2001;
 
+    // cout << digit % 10 << endl;
+    // digit = digit / 10;
+    // cout << digit % 10 << endl;
+    // cout << digit % 10 << endl;
+    // digit = digit / 10;
+
+    // cout << digit;
+    int digiLen = 0;
+    while (digit > 0)
+    {
+        cout << digit % 10 << endl;
+        digit = digit / 10;
+        digiLen++;
+    }
+    cout << "DigiLen:" << digiLen;
     // cout << "Enter size of the array:";
     // cin >> size;
     // for (int i = 0; i < size; i++)
     //     cin >> arr[i];
 
     // testFunction(arr, size);
-    char testString[9] = {'S', 't', '\0', 's'};
-    string s = {'a', 'b', '\0', 'b'};
-    cout << testString << "|" << s;
+    // char testString[9] = {'S', 't', '\0', 's'};
+    // string s = {'a', 'b', '\0', 'b'};
+    // cout << testString << "|" << s;
     return 0;
 }
