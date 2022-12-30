69cd3cc3b9e83165413b615a202d643c732977a3
santoshgopane <santoshgopane68@gmail.com>
2022-12-30 23:57:39 +0530

    DSASheet - Kth Smallest element.

---
+++ DSASheet/Arrays/Problem3LinearTime.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    return 0;
+}
+
+int main()
+{
+    int arr[99] = {7, 10, 4, 3, 20, 15};
+    int l = 0, r = 5, k = 3;
+    cout << "Original Array: ";
+    for (int i = 0; i < 6; i++)
+        cout << arr[i] << " ";
+    cout << endl
+         << k << "th smallest element is: " << kthSmallest(arr, l, r, k);
+}
