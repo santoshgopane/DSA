4f814e513b25d94b1a3ecc6f0d062e79bee6dea6
santoshgopane <santoshgopane68@gmail.com>
2023-01-08 13:04:42 +0530

    TC failed for Minimum jump

--- DSASheet/Arrays/Problem10.cpp
+++ DSASheet/Arrays/Problem10.cpp
@@ -1,3 +1,4 @@
+// Wrong logic
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,15 +8,19 @@ int minJumps(int arr[], int n)
     int jumps = 0;
     while (i <= n - 1)
     {
+        cout << "i: " << i << endl;
+        if (arr[i] == 0)
+            return -1;
         i += arr[i];
         jumps++;
     }
+    cout << "jumps: " << jumps << endl;
     return (i >= n - 1) ? jumps : -1;
 }
 
 int main()
 {
-    int arr[99] = {1};
-    int n = 1;
+    int arr[99] = {2, 3, 1, 1, 2, 4, 2, 0, 1, 1};
+    int n = 10;
     cout << "Minimum Jumps: " << minJumps(arr, n);
 }
